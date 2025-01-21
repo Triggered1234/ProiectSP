4e8fc3fa2bc4f1d2008db32e6e74817508cae437
Triggered1234 <adrian.enache03@e-uvt.ro>
2025-01-21 06:07:43 +0200

    Commit

---
+++ src/test/java/com/example/ilie_sp2/IlieSp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProiectSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlieSp2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
